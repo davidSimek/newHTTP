c7e215d1bff0f06dc417eb3e41cd7ceef2a946b2
davidSimek <david.simek@infis.cz>
2024-01-24 21:57:39 +0100

    need to parse headers fully, split them into pairs

--- src/i_impl.c
+++ src/i_impl.c
@@ -5,7 +5,10 @@
 
 #define METHOD  0
 #define PATH    1
-#define CLEANUP 10
+#define VERSION 2
+#define HEADERS 3
+#define DATA    4
+
 
 void i_parse_request(Request* request) {
     log_info("parsing");
@@ -36,13 +39,17 @@ void i_parse_request(Request* request) {
     // result stings
     char* method;
     char* path;
+    char* version;
 
     // Dynamic String for method
     String method_s;
     create_string(&method_s);
     String path_s;
     create_string(&path_s);
-
+    String version_s;
+    create_string(&version_s);
+    StringView headers_s;
+    char* headers_start;
 
     int part = METHOD;
     while (current_char != 0) {
@@ -61,6 +68,7 @@ void i_parse_request(Request* request) {
             } else {
                 add_char(&method_s, current_char);
             }
+        // PATH PARSING
         } else if (part == PATH) {
             printf("current char: %c\n", current_char);
             if (current_char == ' ') {
@@ -70,12 +78,43 @@ void i_parse_request(Request* request) {
                     break;
                 }
                 copy_string(&path_s, path, string_lenght(&path_s));
-                part = 4;
+                part = VERSION;
             } else {
                 add_char(&path_s, current_char);
             }
+        // VERSION PARSING
+        } else if (part == VERSION) {
+            printf("current char: %c\n", current_char);
+            if ( position[0] == '\r' &&
+                &position[1] != NULL &&
+                 position[1] == '\n') {
+                version = malloc(string_lenght(&version_s)); 
+                if (version == NULL) {
+                    request->is_valid = false;
+                    break;
+                }
+                copy_string(&version_s, version, string_lenght(&version_s));
+                part = HEADERS;
+                ++position;
+                headers_start = position += 1;
+            } else {
+                add_char(&version_s, current_char);
+            }
+        // find where headers start and end
+        } else if (part == HEADERS) {
+            if ( position[0] == '\r' &&
+                &position[1] != NULL &&
+                 position[1] == '\n' &&
+                &position[2] != NULL &&
+                 position[2] == '\r' &&
+                &position[3] != NULL &&
+                 position[3] == '\n') {
+                create_string_view(&headers_s, headers_start, position);
+                part = DATA;
+            }
+        } else if (part == DATA) {
+            
         }
-
 
 
         // next iteration setup
@@ -90,11 +129,17 @@ void i_parse_request(Request* request) {
     // assign
     request->method = method;
     request->path = path;
+    request->http_version = version;
     log_info(request->method);
     log_info(request->path);
+    log_info(request->http_version);
+    printf("headers are:\n");
+    print_string_view(&headers_s);
 
     // cleanup
     destroy_string(&method_s);
+    destroy_string(&path_s);
+    destroy_string(&version_s);
 }
 
 void i_preview_request(Request* request) {
